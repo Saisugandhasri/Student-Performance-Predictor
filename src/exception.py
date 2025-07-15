bb9bedbfceffb07743aea83106bf59e8165b5c4d
Saisugandhasri <saidivya282006@gmail.com>
2025-07-15 10:54:39 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,9 @@
 import sys
-import logging
+from src.logger import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name =exc_tb.tb_frame.f_code.co_filename
+def error_message_detail(error,error_detail:sys): #This function takes: error: the actual exception (e.g., ZeroDivisionError) & error_detail: the full error context from sys (used to get traceback)
+    _,_,exc_tb=error_detail.exc_info() # This gets the traceback object exc_tb from the exception where exc_info() gives: (exception_type, exception_object, traceback)
+    file_name =exc_tb.tb_frame.f_code.co_filename #Gets the name of the Python file where the error occurred
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
@@ -11,12 +11,14 @@ def error_message_detail(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super().__init__(error_message)
+        super().__init__(error_message) #Calls base class Exception to store the original message
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
     
+
+
 
     
 
--- src/logger.py
+++ src/logger.py
@@ -1,16 +1,24 @@
-import logging
+import logging # Imports Python's built-in logging module – This allows us to record messages (info, warnings, errors) to a file or console
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" #Creates a unique log file name based on the current date and time
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE) #os.getcwd() → gets the current working directory and os.path.join(...) → safely joins folder names to build the path
 os.makedirs(logs_path,exist_ok=True)
 
 LOG_FILE_PATH =os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s", 
+# | Part            | Meaning                              |
+# | --------------- | ------------------------------------ |
+# | `%(asctime)s`   | Time when the log was recorded       |
+# | `%(lineno)d`    | Line number where logging was called |
+# | `%(name)s`      | Name of the logger/module            |
+# | `%(levelname)s` | INFO, DEBUG, WARNING, ERROR, etc.    |
+# | `%(message)s`   | The actual message you write         |
+
     level=logging.INFO,
 
 )
