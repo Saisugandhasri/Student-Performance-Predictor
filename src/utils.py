6ce90fdb7ec1c8ba2fa73ca611e65c0117a61db2
Saisugandhasri <saidivya282006@gmail.com>
2025-07-18 05:44:45 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill #dill: Like pickle, but more powerful — it can serialize more complex Python objects.
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,11 +21,17 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+
 
             model.fit(X_train,y_train) #train model
 
